d62238134d94a57516c8f0bbda25e9a7e7f6e783
Кирилл Нестерович <nesterkalol@gmail.com>
2022-04-20 18:17:51 +0300

    Multiple changes in flyout

---
+++ EternityApp/EternityApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EternityApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            InfoLabel.Text = "Дипломный проект\n" +
+                "выпускницы группы 32о 2022 года\n" +
+                "Панфиловой Дианы Дмитриевны";
+        }
+    }
+}
